23129f6344eb84208a4279d1d64f91615fc3c0f4
Aman Linux <yo.akamangmail.com>
2024-07-04 23:04:26 +0530

    update

---
+++ JAVA/oops/Static/bluePrint.java
@@ -0,0 +1,17 @@
+public class bluePrint {
+
+    public static int count = 0;
+
+    public bluePrint(){
+        count++;
+    }
+    sout
+}
+
+public class bluePrints{
+    public static void main(String[] args) {
+        bluePrint obj = new bluePrint();
+       
+    }
+}
+
---
+++ JAVA/oops/Static/stat2.java
@@ -0,0 +1,9 @@
+public class stat2 {
+    public static void main(String[] args) {
+        stat obj = new stat();
+        // obj.count;
+        System.out.println(obj.count);
+
+        stat.heyyy();
+    }
+}
